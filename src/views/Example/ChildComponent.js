9572b87eea62d0773c00a72d32b9738917d5c467
Nam Van <nam.van@ap.omron.net>
2022-09-22 21:34:34 +0700

    init project

--- src/views/Example/ChildComponent.js
+++ src/views/Example/ChildComponent.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import './Demo.scss'
 class ChildComponent extends React.Component {
     state = {
         showJobs: false
@@ -20,7 +20,10 @@ class ChildComponent extends React.Component {
             <>
                 {showJobs === false ?
                     <div>
-                        <button onClick={() => this.handleShowHide()}>Show</button>
+                        <button className="btn-show"
+                            onClick={() => this.handleShowHide()}>
+                            Show
+                        </button>
                     </div>
                     :
                     <>
